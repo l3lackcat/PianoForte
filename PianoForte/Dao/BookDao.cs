1f770f6a2901ccba777d62cc621d45b2b5e202a7
Suphakiat Lohasammakul <l3lackcat.g@gmail.com>
2014-01-17 00:04:23 +0700

    Setup Project

---
+++ PianoForte/Dao/BookDao.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using PianoForte.Models;
+
+namespace PianoForte.Dao
+{
+    public interface BookDao
+    {
+        bool insertBook(string databaseName, Book book);
+
+        bool updateBook(string databaseName, Book book);
+
+        Book getBook(string databaseName, int bookId);
+
+        Book getLastBook(string databaseName);
+
+        List<Book> getBookList(string databaseName);
+
+        List<Book> getBookList(string databaseName, int startIndex, int offset);
+
+        List<Book> getBookList(string databaseName, int startIndex, int offset, string keyword);
+
+        List<Book> getBookListByName(string databaseName, string bookName);
+    }
+}
