5007339123f88a3ae7136e2436f59341ff451420
Suphakiat Lohasammakul <l3lackcat.g@gmail.com>
2014-01-17 00:06:32 +0700

    Setup Project

---
+++ PianoForte/Models/Holiday.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PianoForte.Models
+{
+    public class Holiday
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Comment { get; set; }
+    }
+}
---
+++ PianoForte/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using PianoForte.Enum;
+
+namespace PianoForte.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Nickname { get; set; }
+        public Status Status { get; set; }
+    }
+}
