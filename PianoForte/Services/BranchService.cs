c71d9096e326c4670049836b359c61c0ad07dcc9
Suphakiat Lohasammakul <l3lackcat.g@gmail.com>
2014-01-17 00:07:41 +0700

    Setup Project

---
+++ PianoForte/Services/BranchService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using PianoForte.Dao;
+using PianoForte.Models;
+
+namespace PianoForte.Services
+{
+    public class BranchService
+    {
+        private static BranchDao branchDao = DaoFactory.getDaoFactory(DaoFactory.FactoryType.MYSQL).getBranchDao();
+
+        public static bool insertBranch(Branch branch)
+        {
+            return branchDao.insertBranch(branch);
+        }
+
+        public static bool updateBranch(Branch branch)
+        {
+            return branchDao.updateBranch(branch);
+        }
+
+        public static Branch getBranch(int branchId)
+        {
+            Branch branch = null;
+
+            List<Branch> tempBranchList = branchDao.getBranchList(branchId);
+            if (tempBranchList.Count == 1)
+            {
+                branch = tempBranchList[0];
+            }
+
+            return branch;
+        }
+
+        public static List<Branch> getBranchList()
+        {
+            return branchDao.getBranchList();
+        }
+    }
+}
---
+++ PianoForte/Services/CdService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using PianoForte.Dao;
+using PianoForte.Models;
+
+namespace PianoForte.Services
+{
+    public class CdService
+    {
+        private static CdDao cdDao = DaoFactory.getDaoFactory(DaoFactory.FactoryType.MYSQL).getCdDao();
+
+        public static int getNewCdId(string databaseName)
+        {
+            int newCdId = 0;
+
+            Cd cd = cdDao.getLastCd(databaseName);
+            if (cd != null)
+            {
+                newCdId = cd.Id + 1;
+            }
+            else
+            {
+                newCdId = ((int)Enum.ProductType.CD * 1000000) + 1;
+            }
+
+            return newCdId;
+        }
+
+        public static bool insertCd(string databaseName, Cd cd)
+        {
+            return cdDao.insertCd(databaseName, cd);
+        }
+
+        public static bool updateCd(string databaseName, Cd cd)
+        {
+            return cdDao.updateCd(databaseName, cd);
+        }
+
+        public static Cd getCd(string databaseName, int cdId)
+        {
+            return cdDao.getCd(databaseName, cdId);
+        }
+
+        public static Cd getLastCd(string databaseName)
+        {
+            return cdDao.getLastCd(databaseName);
+        }
+
+        public static List<Cd> getCdList(string databaseName)
+        {
+            return cdDao.getCdList(databaseName);
+        }
+
+        public static List<Cd> getCdList(string databaseName, int startIndex, int offset)
+        {
+            return cdDao.getCdList(databaseName, startIndex, offset);
+        }
+
+        public static List<Cd> getCdList(string databaseName, int startIndex, int offset, string keyword)
+        {
+            return cdDao.getCdList(databaseName, startIndex, offset, keyword);
+        }
+
+        public static List<Cd> getCdListByName(string databaseName, string cdName)
+        {
+            return cdDao.getCdListByName(databaseName, cdName);
+        }
+    }
+}
