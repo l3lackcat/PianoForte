d28b781335cd1f5ea756c49565c0ac9518f87fb0
Suphakiat Lohasammakul <l3lackcat.g@gmail.com>
2014-01-17 00:07:49 +0700

    Setup Project

---
+++ PianoForte/Services/HolidayService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using PianoForte.Dao;
+using PianoForte.Models;
+
+namespace PianoForte.Services
+{
+    public class HolidayService
+    {
+        private static HolidayDao holidayDao = DaoFactory.getDaoFactory(DaoFactory.FactoryType.MYSQL).getHolidayDao();
+
+        public static bool insertHoliday(string databaseName, Holiday holiday)
+        {
+            return holidayDao.insertHoliday(databaseName, holiday);
+        }
+
+        public static bool updateHoliday(string databaseName, Holiday holiday)
+        {
+            return holidayDao.updateHoliday(databaseName, holiday);
+        }
+
+        public static Holiday getHolidayt(string databaseName, int holidayId)
+        {
+            Holiday holiday = null;
+
+            List<Holiday> tempHolidayList = holidayDao.getHolidayList(databaseName, holidayId);
+            if (tempHolidayList.Count == 1)
+            {
+                holiday = tempHolidayList[0];
+            }
+
+            return holiday;
+        }
+
+        public static List<Holiday> getHolidayList(string databaseName)
+        {
+            return holidayDao.getHolidayList(databaseName);
+        }
+    }
+}
---
+++ PianoForte/Services/OtherProductService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using PianoForte.Dao;
+using PianoForte.Models;
+
+namespace PianoForte.Services
+{
+    public class OtherProductService
+    {
+        private static OtherProductDao otherProductDao = DaoFactory.getDaoFactory(DaoFactory.FactoryType.MYSQL).getOtherProductDao();
+
+        public static bool insertOtherProduct(string databaseName, OtherProduct otherProduct)
+        {
+            return otherProductDao.insertOtherProduct(databaseName, otherProduct);
+        }
+
+        public static bool updateOtherProduct(string databaseName, OtherProduct otherProduct)
+        {
+            return otherProductDao.updateOtherProduct(databaseName, otherProduct);
+        }
+
+        public static OtherProduct getOtherProduct(string databaseName, int otherProductId)
+        {
+            OtherProduct otherProduct = null;
+
+            List<OtherProduct> tempOtherProductList = otherProductDao.getOtherProductList(databaseName, otherProductId);
+            if (tempOtherProductList.Count == 1)
+            {
+                otherProduct = tempOtherProductList[0];
+            }
+
+            return otherProduct;
+        }
+
+        public static List<OtherProduct> getOtherProductList(string databaseName, int startIndex, int offset, string keyword)
+        {
+            return otherProductDao.getOtherProductList(databaseName, startIndex, offset, keyword);
+        }
+    }
+}
