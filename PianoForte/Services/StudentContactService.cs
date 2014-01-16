317794fd4a6c4ed78440a417a5c6a4d65c75e635
Suphakiat Lohasammakul <l3lackcat.g@gmail.com>
2014-01-17 00:08:04 +0700

    Setup Project

---
+++ PianoForte/Services/StudentContactService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using PianoForte.Dao;
+using PianoForte.Enum;
+using PianoForte.Models;
+
+namespace PianoForte.Services
+{
+    public class StudentContactService
+    {
+        private static StudentContactDao studentContactDao = DaoFactory.getDaoFactory(DaoFactory.FactoryType.MYSQL).getStudentContactDao();
+
+        public static bool insertStudentContact(string databaseName, StudentContact studentContact)
+        {
+            return studentContactDao.insertStudentContact(databaseName, studentContact);
+        }
+
+        public static bool updateStudentContact(string databaseName, StudentContact studentContact)
+        {
+            return studentContactDao.updateStudentContact(databaseName, studentContact);
+        }
+
+        public static bool deleteStudentContact(string databaseName, int contactId)
+        {
+            return studentContactDao.deleteStudentContact(databaseName, contactId);
+        }
+
+        public static bool deleteStudentContactList(string databaseName, int studentId)
+        {
+            return studentContactDao.deleteStudentContactList(databaseName, studentId);
+        }
+
+        public static StudentContact getStudentContact(string databaseName, int contactId)
+        {
+            return studentContactDao.getStudentContact(databaseName, contactId);
+        }
+
+        public static StudentContact getStudentContact(string databaseName, int studentId, ContactType type, string label, string content)
+        {
+            return studentContactDao.getStudentContact(databaseName, studentId, type, label, content);
+        }
+
+        public static List<StudentContact> getStudentContactList(string databaseName, int studentId)
+        {
+            return studentContactDao.getStudentContactList(databaseName, studentId);
+        }
+
+        public static List<StudentContact> getStudentContactList(string databaseName, int studentId, Status status)
+        {
+            return studentContactDao.getStudentContactList(databaseName, studentId, status);
+        }
+    }
+}
---
+++ PianoForte/Services/TeachedCourseService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using PianoForte.Dao;
+
+namespace PianoForte.Services
+{
+    public class TeachedCourseService
+    {
+        private static TeachedCourseDao teachedCourseDao = DaoFactory.getDaoFactory(DaoFactory.FactoryType.MYSQL).getTeachedCourseDao();
+
+        public static bool insertTeachedCourse(string databaseName, int teacherId, int courseId)
+        {
+            return teachedCourseDao.insertTeachedCourse(databaseName, teacherId, courseId);
+        }
+
+        public static bool deleteTeachedCourseListByCourseId(string databaseName, int courseId)
+        {
+            return teachedCourseDao.deleteTeachedCourseListByCourseId(databaseName, courseId);
+        }
+
+        public static bool deleteTeachedCourseListByTeacherId(string databaseName, int teacherId)
+        {
+            return teachedCourseDao.deleteTeachedCourseListByTeacherId(databaseName, teacherId);
+        }
+
+        public static List<int> getTeachedCourseIdList(string databaseName, int teacherId)
+        {
+            return teachedCourseDao.getTeachedCourseIdList(databaseName, teacherId);
+        }
+    }
+}
