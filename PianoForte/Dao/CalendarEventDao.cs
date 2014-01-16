ae842eccac309187560b0c0b1675d369bd43cbae
Suphakiat Lohasammakul <l3lackcat.g@gmail.com>
2014-01-17 00:05:02 +0700

    Setup Project

---
+++ PianoForte/Dao/CalendarEventDao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using PianoForte.Models;
+
+namespace PianoForte.Dao
+{
+    public interface CalendarEventDao
+    {
+        //List<CalendarEvent> getCalendarEvents(int teacherId, DateTime startDate, DateTime endDate);
+    }
+}
