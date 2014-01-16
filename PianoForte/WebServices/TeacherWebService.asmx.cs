59ca4bf3c4791fd35b6d88911c4f496470fbd08c
Suphakiat Lohasammakul <l3lackcat.g@gmail.com>
2014-01-17 00:11:10 +0700

    Setup Project

---
+++ PianoForte/WebServices/TeacherWebService.asmx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+using PianoForte.Enum;
+using PianoForte.Models;
+using PianoForte.Services;
+
+namespace PianoForte.WebServices
+{
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    [System.Web.Script.Services.ScriptService]
+    public class TeacherWebService : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public Teacher getTeacherById(string databaseName, int teacherId)
+        {
+            //System.Threading.Thread.Sleep(3000);
+
+            Teacher teacher = TeacherService.getTeacher(databaseName, teacherId);
+            if (teacher != null)
+            {
+                //Contact list
+                teacher.ContactList = TeacherContactService.getTeacherContactList(databaseName, teacherId, Status.ACTIVE);
+
+                //Teached course list
+                List<int> teachedCourseIdList = TeachedCourseService.getTeachedCourseIdList(databaseName, teacherId);
+                foreach (int teachedCourseId in teachedCourseIdList)
+                {
+                    Course course = CourseService.getCourse(databaseName, teachedCourseId);
+                    if (course != null)
+                    {
+                        if (!teacher.TeachedCourseList.Contains(course.Name))
+                        {
+                            teacher.TeachedCourseList.Add(course.Name);
+                        }
+                    }
+                }
+            }
+
+            return teacher;
+        }
+    }
+}
