74648551b06c74db8fc3ad43276d2aa9d53ebc4a
Suphakiat Lohasammakul <l3lackcat.g@gmail.com>
2014-01-17 00:08:56 +0700

    Setup Project

---
+++ PianoForte/Web/App/js/services/teachers/TeacherService.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+goog.provide('PianoForte.Services.Teachers.TeacherService');
+
+PianoForte.Services.Teachers.TeacherService = function ($http) {
+    return {
+        getTeacherInfoById: function (teacherId, onSuccess, onError) {
+            var data = {
+                databaseName: 'pianoforte_b01',
+                teacherId: teacherId
+            };
+
+            $http.post('/WebServices/TeacherWebService.asmx/getTeacherById', data).success(onSuccess).error(onError);
+        }
+    }
+}
