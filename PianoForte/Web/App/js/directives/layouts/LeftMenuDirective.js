dd6d42a20bfc054dc1dd39bcc05027d3fc41d03a
l3lackcat <l3lackcat@hotmail.com>
2014-01-23 19:34:08 +0700

    Modify - Style
    Add
    - Left Menu

---
+++ PianoForte/Web/App/js/directives/layouts/LeftMenuDirective.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+goog.provide('PianoForte.Directives.Layouts.LeftMenuDirective');
+
+PianoForte.Directives.Layouts.LeftMenuDirective = function () {
+    return {
+        restrict: 'E',
+        transclude: true,
+        scope: {
+            title: '@',
+            editable: '@',
+            edit: '&'
+        },
+        controller: 'LeftMenuController',
+        templateUrl: 'partials/layouts/left-menu.htm'
+    };
+}
