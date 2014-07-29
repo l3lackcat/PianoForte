494666a58a794d1e29a7ba2690320659470ec327
Suphakiat Lohasammakul <l3lackcat.g@gmail.com>
2014-07-30 00:11:03 +0700

    Modified Gruntfile.js

--- Web Application 2/pianoforte/Gruntfile.js
+++ Web Application 2/pianoforte/Gruntfile.js
@@ -1,14 +1,130 @@
 module.exports = function(grunt) {
 	'use strict';
 
+  require('load-grunt-tasks')(grunt);
+
 	grunt.initConfig({
 		meta: {
 			app: 'app',
 			dist: 'dist',
 			server: 'server',
 			distapp: '<%= meta.dist %>/<%= meta.app %>',
+      bower_components: '<%= meta.app %>/bower_components',
 			pkg: grunt.file.readJSON('package.json'),
+      banner: ['/*',
+               ' * <%= meta.pkg.name %>',
+               ' * Version: <%= meta.pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>',
+               ' */\n'].join('\n')
 		},    
+
+    clean: {
+      common: ['<%= meta.dist %>', '<%= meta.app %>/**/*.html.js'],
+      nonmin: ['<%= meta.distapp %>/*.js', '!<%= meta.distapp %>/*.min.js', '<%= meta.distapp %>/*.css', '!<%= meta.distapp %>/*.min.css']
+    },
+
+    concat: {
+      options: {
+        banner: '<%= meta.banner %>'
+      },
+      prejs: {
+        src: '<%= dom_munger.data.prejs %>',
+        dest: '<%= meta.distapp %>/pre.js'
+      },
+      appjs: {
+        src: ['<%= meta.distapp %>/app.js', '<%= dom_munger.data.appjs %>'],
+        dest: '<%= meta.distapp %>/app.js'
+      },
+      libjs: {
+        src: '<%= dom_munger.data.libjs %>',
+        dest: '<%= meta.distapp %>/lib.js'
+      },
+      libcss: {
+        src: '<%= dom_munger.data.libcss %>',
+        dest: '<%= meta.distapp %>/lib.css'
+      }
+    },
+
+    connect: {   
+      options: {
+        port: 9000,
+        livereload: 35729,
+        hostname: 'localhost'
+      },
+      dev: {
+        options: {
+          open: {
+            target: 'http://localhost:9000/app.html'
+          },
+          base: 'dist/app'
+        }
+      }
+    },
+
+    copy: {
+      dev: {
+        files: [
+          {
+            expand: true,
+            src: ['<%= meta.app %>/**', '!<%= meta.bower_components %>/**', 'app.json'],
+            dest: '<%= meta.dist %>/',
+            filter: 'isFile'
+          }, {
+            expand: true,
+            cwd: '<%= meta.server %>/',
+            src: ['**', '!server.js'],
+            dest: '<%= meta.dist %>/',
+            filter: 'isFile'
+          }, {
+            src: ['<%= meta.server %>/server.js'],
+            dest: '<%= meta.dist %>/app.js'
+          }
+        ]
+      },
+      bower: {
+        files: [
+          {
+            expand: true,
+            src: ['<%= meta.bower_components %>/**'],
+            dest: '<%= meta.dist %>/',
+            filter: 'isFile'
+          }
+        ]
+      },
+      dist: {
+        files: [
+          {
+            src: 'app.json',
+            dest: '<%= meta.dist %>/app.json'
+          }, {
+            expand: true,
+            cwd: '<%= meta.server %>/',
+            src: ['**', '!server.js'],
+            dest: '<%= meta.dist %>/',
+            filter: 'isFile'
+          }, {
+            src: ['<%= meta.server %>/server.js'],
+            dest: '<%= meta.dist %>/app.js'
+          }, {
+            expand: true,
+            src: '<%= meta.staticfolders %>',
+            dest: '<%= meta.dist %>/'
+          }
+        ]
+      }
+    },
+
+    cssmin: {
+      dist: {
+        options: {
+          banner: '<%= meta.banner %>'
+        },
+        expand: true,
+        cwd: '<%= meta.distapp %>/',
+        src: ['**/*.css', '!bower_components/**'],
+        dest: '<%= meta.distapp %>/',
+        ext: '.min.css'
+      }
+    },
 
 		dom_munger: {
       dist: {
@@ -32,14 +148,6 @@ module.exports = function(grunt) {
             }
           ],
           remove: ['[data-build]'],
-          prepend: [
-            {
-              selector: 'body',
-              html: [
-                '<script src="pre.min.js"></script>',
-                '\n'].join('\n')
-            }
-          ],
           append: [
             {
               selector: 'body',
@@ -90,12 +198,23 @@ module.exports = function(grunt) {
     			'<%= meta.distapp %>/app.css': '<%= meta.app %>/app.less'
     		}
     	}
+    },
+
+    watch: {
+      css: {
+        files: ['<%= meta.app %>/**/*.less'],
+        tasks: ['buildcss']
+      },
+      other: {
+        files: ['<%= meta.app %>/**', '<%= meta.server %>/**', '!<%= meta.app %>/**/*.less'],
+        tasks: ['check', 'copy:dev']
+      }
     }
   });
 
-	grunt.registerTask('buildcss', ['less']);
+	grunt.registerTask('buildcss', ['less', 'cssmin']);
 	grunt.registerTask('buildjs', ['test', 'dom_munger:dist', 'html2js:dist', 'concat', 'uglify']);
 	grunt.registerTask('check', ['jshint', 'jscs']);
 
-  grunt.registerTask('dev', [])
+  grunt.registerTask('dev', ['clean:common', 'copy:dev', 'copy:bower', 'buildcss', 'connect', 'watch']);
 }
--- Web Application 2/pianoforte/app/app.js
+++ Web Application 2/pianoforte/app/app.js
@@ -1,37 +1,53 @@
-angular.module('curve-builder', [
-  'ngAnimate',
-  'ngSanitize',
-  'ngGrid',
-  'ng-context-menu',
-  'ui.bootstrap',
-  'ui.tree',
-  'calc',
-  'pascalprecht.translate',
-  'tmh.dynamicLocale',
-  'curve-builder-tpls'
+angular.module('pianoforte', [
+  'ngRoute'
 ])
-  .config(['$httpProvider', '$translateProvider', 'tmhDynamicLocaleProvider', function ($httpProvider, $translateProvider, tmhDynamicLocaleProvider) {
-    $httpProvider.interceptors.push('httpInterceptor');
-    $translateProvider.useStaticFilesLoader({
-      prefix: 'localization/',
-      suffix: '.json'
-    });
-    $translateProvider.preferredLanguage('en');
-    tmhDynamicLocaleProvider.localeLocationPattern('/Apps/calculatorng/file?angular-i18n/angular-locale_{{locale}}.js');
-  }])
-  .run(['$window', '$http', '$timeout', 'User', 'Localization', 'NProgress', function ($window, $http, $timeout, User, Localization, NProgress) {
-    // Remove Eikon scrollbar
-    $window.top.document.body.style.position = 'fixed';
-
-    NProgress.configure({ showSpinner: false });
-    User.init();
-
-    // UserSettings service request slows down other request
-    $timeout(function () {
-      User.getSettings().success(function (userSettings) {
-        Localization.set(userSettings['COMMON.REGIONAL_SETTINGS.UI_LANGUAGE'],
-          userSettings['COMMON.REGIONAL_SETTINGS.NUMBERFORMAT_DECIMALSEPARATOR'],
-          userSettings['COMMON.REGIONAL_SETTINGS.NUMBERFORMAT_GROUPSEPARATOR']);
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        controller: 'Teachers.MainController',
+        templateUrl: 'partials/teachers/main.htm'
+      })
+      .when('/books', {
+        controller: 'Books.MainController',
+        templateUrl: 'partials/books/main.htm'
+      })
+      .when('/books/:bookId', {
+        controller: 'Books.AboutController',
+        templateUrl: 'partials/books/about.htm'
+      })
+      .when('/cds', {
+        controller: 'Cds.MainController',
+        templateUrl: 'partials/cds/main.htm'
+      })
+      .when('/cds/:cdId', {
+        controller: 'Cds.AboutController',
+        templateUrl: 'partials/cds/about.htm'
+      })
+      .when('/courses', {
+        controller: 'Courses.MainController',
+        templateUrl: 'partials/courses/main.htm'
+      })
+      .when('/courses', {
+        controller: 'Courses.AboutController',
+        templateUrl: 'partials/courses/about.htm'
+      })
+      .when('/students', {
+        controller: 'Students.MainController',
+        templateUrl: 'partials/students/main.htm'
+      })
+      .when('/students/:studentId', {
+        controller: 'Students.AboutController',
+        templateUrl: 'partials/students/about.htm'
+      })
+      .when('/teachers', {
+        controller: 'Teachers.MainController',
+        templateUrl: 'partials/teachers/main.htm'
+      })
+      .when('/teachers/:teacherId', {
+        controller: 'Teachers.AboutController',
+        templateUrl: 'partials/teachers/about.htm'
+      })
+      .otherwise({
+        redirectTo: '/'
       });
-    }, 100);
   }]);
